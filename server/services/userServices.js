28adcc26a1b2f653e7703a285aae85ba1e343913
sandip <sandip.pandit230496@gmail.com>
2024-03-31 16:33:07 +0530

    added functionality -> signin,signup,review,comments,bookmark

---
+++ server/services/userServices.js
@@ -0,0 +1,23 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+
+exports.getUserByemail = async (email) => {
+    const user = await User.findOne({ email });
+    return user;
+};              
+
+exports.createUser = async ({ email, password }) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    return newUser;
+};  
+
+exports.bookmark=async (userId, bookId) => {
+    const user = await User.findById(userId);
+    user.bookmarkedBooks.push(bookId);
+    await user.save();
+    return user;
+};
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import BookList from './components/bookList/BookList';
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
+import BookDetails from './components/bookDetails/BookDetails';
+import { Provider } from 'react-redux';
+import LoginPage from './components/Login/Login';
+import store from './redux/store';
+import Favorite from './components/Favorite/Favorite';
+import Home from './components/HomeComponent/Home';
+import NavBar from './components/navBar/NavBar';
+import Register from './components/Register/Register';
+import ProtectedRoutes from './components/Routes/ProtectedRoutes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const token = localStorage.getItem('token');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer/>
+      <Provider store={store}>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<Register />} />
+             <Route element={<ProtectedRoutes />}>
+                <Route path='/books' element={<Home />}>
+                  <Route index element={<BookList />} />
+                </Route>
+                <Route path='/favorites' element={<Favorite />} />
+                <Route path='/book_details/:id' element={<BookDetails />} />
+              </Route>
+            
+          </Routes>
+      </Provider>
     </div>
   );
 }
